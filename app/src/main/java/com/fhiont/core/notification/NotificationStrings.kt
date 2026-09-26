249e1e683a62581f1f36d58aa631a321d9dd275c
fhiontrealtyllp-beep <RealeApp@101>
2026-09-26 10:25:10 +0530

    notify enquiry

---
+++ app/src/main/java/com/fhiont/core/notification/NotificationStrings.kt
@@ -0,0 +1,9 @@
+package com.fhiont.core.notification
+
+internal object NotificationStrings {
+    const val PERMISSION_RATIONALE_TITLE = "Enable notifications"
+    const val PERMISSION_RATIONALE_MESSAGE =
+        "Allow notifications so you are alerted when someone enquires about your property, even when the app is in the background."
+    const val PERMISSION_ALLOW = "Allow"
+    const val PERMISSION_NOT_NOW = "Not now"
+}
